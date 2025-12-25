c364393c770eca9f4f05fa17e0d5e460dcfe419f
Hardik Jain <hardikjain1704@gmail.com>
2025-12-25 17:10:53 +0530

    corrected model_loader.py

--- prod_assistant/utils/model_loader.py
+++ prod_assistant/utils/model_loader.py
@@ -10,7 +10,7 @@ from logger import GLOBAL_LOGGER as log
 from exception.custom_exception import ProductAssistantException
 import asyncio
 
-
+load_dotenv()
 class ApiKeyManager:
     def __init__(self):
         self.api_keys = {
